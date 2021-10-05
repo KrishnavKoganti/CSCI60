7b475e8026d300ba4e969435de04d6a5e375ea95
Krishna Koganti <kvk141620@gmail.com>
2021-10-05 12:58:48 -0700

    Classwork 10.4

--- Inclass/10.1/bag.cpp
+++ Inclass/10.1/bag.cpp
@@ -1,46 +1,86 @@
 #include "bag.h"
+#include <cassert>
 
-Bag::Bag(){
-  size_=0;
-  for(std::size_t i = 0; i<CAPACITY; ++i){
+Bag::Bag() {
+  size_ = 0;
+  for (std::size_t i = 0; i < CAPACITY; ++i) {
     data_[i] = 0;
   }
 }
 
-Bag::Bag(int arr[], std::size_t size){
-  assert(size<CAPACITY);
-  size_=size;
-  for(std::size_t i = 0; i<CAPACITY; ++i){
+Bag::Bag(int arr[], std::size_t size) {
+  assert(size < CAPACITY);
+  size_ = size;
+  for (std::size_t i = 0; i < CAPACITY; ++i) {
     data_[i] = arr[i];
   }
 }
 
-std::size_t Bag::count(int target) const{
+std::size_t Bag::count(int target) const {
   std::size_t count = 0;
-  for(std::size_t i=0; i< size_; ++i){
-    if(data_[i]==target)
+  for (std::size_t i = 0; i < size_; ++i) {
+    if (data_[i] == target)
       ++count;
   }
   return count;
 }
 
-void Bag::insert(int target){
+void Bag::insert(int target) {
   assert(size() < CAPACITY);
   data_[size()] = target;
   ++size_;
-
 }
 
-// pre: size() + b.size() <= CAPACITY
-// post: adds a copy of each element of b to this Bag
-void operator += (const Bag & b);
+void Bag::operator+=(const Bag &b) {
+  assert(size() + b.size() <= CAPACITY);
+  std::size_t j = size_;
+  for (std::size_t i = size_; i < b.size_; ++i) {
+    data_[j] = b[i];
+    ++j;
+  }
+  size_ = size_ + b.size_;
+}
 
-// pre: none
-// post: if target appears in this Bag, removes one copy of target and returns true
-//       else returns false
-bool erase_one(int target);
+bool Bag::erase_one(int target) {
 
-// pre: none
-// post: if target appears in this Bag k times, removes all copies and returns k
-//       else returns 0
-std::size_t erase(int target);
+  if (target < size_) {
+    std::size_t marker = -1;
+    for (std::size_t i = 0; i < size_ && marker == -1; ++i) {
+      if (data_[i] == target) {
+        marker = i;
+      }
+    }
+    if (marker == -1) {
+      return false;
+    }
+    for (std::size_t i = marker; i < size_ - 1; ++i) {
+      data_[i] = data_[i + 1];
+    }
+  }
+  --size_;
+  return true;
+}
+
+std::size_t Bag::erase(int target) {
+  std::size_t count = 0;
+  while (erase_one(target)) {
+    ++count;
+  }
+  return count;
+}
+
+Bag operator + (const Bag & b1, const Bag & b2){
+  Bag output;
+
+  output +=b1;
+  output +=b2;
+
+  return output;
+}
+
+std::ostream& operator<<(std::ostream&  out, const Bag& b){
+  for(std::size_t i = 0; i<b.size(); ++i){
+    out<<b[i]<< " ";
+    return out;
+  }
+}
